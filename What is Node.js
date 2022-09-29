76ebfddd98bc6819be6f667121e86ecd56d520e9
Long Nguyen <56315912+Long1908@users.noreply.github.com>
2022-09-29 20:43:04 +0300

    Create What is Node.js

---
+++ What is Node.js
@@ -0,0 +1,3 @@
+What is Node.js? </br>
+Node.js is a way to work Javascript for backend programming, or more particular, we can do work with JS outside the browser, for example with files, <br>
+hardware and etc.
